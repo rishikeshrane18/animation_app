485c3e20a38b003b2e63990c6d66276c38043624
rishikeshrane18 <62653435+rishikeshrane18@users.noreply.github.com>
2021-03-02 23:40:48 +0530

    app 2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,4 @@ class  extends StatelessWidget {
     return Container();
   }
 }
-class  extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
 
